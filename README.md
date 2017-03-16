a84071b34275965cb807a5b6d400ddad23e70351
Pedro Arias Ruiz <asiganthesunk@gmail.com>
2017-03-16 12:41:54 +0100

    Rarbg WebScapper Updated
    Some issues had been detected while trying to get the info from .php website. Added UserFakeAgents...

--- README.md
+++ README.md
@@ -5,21 +5,22 @@ align="right" width="50">](https://www.python.org/)
 *Read this in other languages: [English](README.md), [Spanish](README.es.md).*
 
 
-##### Description
-Galung-Project it's a concept software to help organize your multimedia content, this program it's being developed mainly in python 2.7
+## Description
 
-Development of the assistance platform for xmbc enviroments where the application excells on helping you retrieve information about the
-multimedia content, also known as metadata about public multimedia files using (http/https) requests and peer to peer protocols. It also
-containts some aditional features such as sincronize reproduction of some content throught the network, re-codec and audio video aligment
-on multimedia files .mkv and .mp4.
+Galung-Project it's a concept software to help organize your multimedia content, this program it's being developed mainly in python 2.7 focusing on building a robust platform.
+
+The main focus of this project it's the development of the assistance platform for xmbc enviroments, where the application excels on helping you retrieve information, also known as 
+metadata, about the multimedia content you already have in your computer, and in some cases it will allow the user to get new content from the public domain. The retrieval of information will come using (http/https) requests and peer to peer protocols.
+
+This software also containts some aditional features such as aligment of multiple audio streams in diferent languages and then join them in one common container, supported formats will be .mkv or .mp4, sincronize reproduction over the network and re-codec video and audio using h265.
+
 
 ## Getting Started
 
-These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. 
 
 
 ## Index of Contents
-
 
 1. [Requirements](#installation)
  * [Dependencies](#dependencies)
@@ -29,56 +30,91 @@ These instructions will get you a copy of the project up and running on your loc
 3. [Usage](#usage)
 4. [License](#license)
 
+
 ## Requirements
-Galung-Project requires [python 2.7.x][python_download_link]  and [pip installer][pip_installer_link] installed in your machine to be able to run install.sh. 
+
+The basic requirements for galung-project are [python 2.7.x][python_download_link]  and [pip installer][pip_installer_link]. Once you already had installed those requirements then next step it's to 
+install the depencies. 
 
 
 ### Dependencies
 
+The following list of libraries are needed to make galung-project work.
+
 * [pysrt][pysrt_link]
 * [langdetect][langdetect_link]
+* [enum][enum_link]
 * [pandas][pandas_link]
 * [numpy][numpy_link]
+* [cython][cython_link]
+* [imdbpy][imdbpy_link]
+* [python_mal][python_mal_link]
 * [tvdb_api][tvdb_api_link]
+* [logging][logging_link]
+* [beautifulsoap4][beautifulsoap4_link]
+* [requests][requests_link]
+
 
 ### Installation
 
-Go to the /path/path and run install.sh, this will donwload all the modules required for the software to run propperly
+Go to the main folder of galung-project and run setup.py. This will download and install all the dependecies found on the requirements.txt, so the sotfware can function propperly.
 
-Install the dependencies to be able to start the software.
-
-```sh
- 
-python-mal (0.1.7)
-pandas (0.19.2)
-pysrt (1.1.1)
-requests (2.13.0)
-beautifulsoup4 (4.5.3)
-enum (0.4.6)
-Cython (0.25.2)
-IMDbPY (5.1.1)
-langdetect (1.0.7)
-logging (0.4.9.6)
-
+```
+pysrt==1.1.1
+langdetect==1.0.7
+enum==0.4.6
+pandas==0.19.2
+Cython==0.25.2
+IMDbPY==5.1.1
+python-mal==0.1.7
+tvdb_api==1.10
+logging==0.4.9.6
+beautifulsoup4==4.5.3
+requests==2.13.0
 
 ```
 
 ### Plugins
 
-Galung-Project it's currently developing this table of plugins 
+Galung-Project will integrate this plugins with an Xmbc Interface. 
 
 
-| Pluguin       | Status        | Show | Movie | Anime |
+| Plugin       | Status        | Show | Movie | Anime |
 |:-------------:|:-------------:|:-----------:|:------------:|:------------:|
-| [FileMapper][readme_fm_link] | Developing | Yes | Yes |  |
+| [FileMapper][readme_fm_link] | Yes | Yes | Yes |  |
 | [ConstellationMapper][readme_cm_link] | Developing | - | - | - | - |
-| [FFmpeg][readme_cm_link] | Developing | - | - | - | - |
+| [FFmpeg][readme_ffmpeg_link] | Developing | - | - | - | - |
+
+#### FileMapper
+
+##### Recognized Formats
+
+Anime Format:
+
+ **[_< Uploader >_ ]** : **_< Anime Name >_** : **_< (Episode | Ep | E | x | - ) 000 >_** : **_< Episode Name >_** : **_< Quality >_**
+
+Movie Format:
+
+ **_< Movie Name >_** : **_< ( Year ) >_** : **_< Movie Flag >_** : **_< Quality >_**
+
+Show Format:
+
+  **_< Show Name >_** : **_< E00S00 >_** : **_< Episode Name >_** : **_< Quality >_**
 
 **[Back to index of contents](#index-of-contents)**
+
 
 ### Usage
 
+```python
+
+from main import filemapper as fm
+
+
+```
+
 **[Back to index of contents](#index-of-contents)**
+
 
 ### License
 
@@ -86,13 +122,22 @@ Galung-Project it's currently developing this table of plugins
 
 
 [readme_fm_link]: <https://github.com/AsiganTheSunk/galung-project/blob/master/trunk/filemapper/README.md>
-[readme_cm_link]: <https://github.com/AsiganTheSunk/galung-project/blob/master/trunk/filemapper/README.md>
+[readme_cm_link]: <https://dummy_link.com>
+[readme_ffmpeg_link]: <https://dummy_link.com>
 
 [pip_installer_link]: <https://pip.pypa.io/en/stable/installing/>
 [python_download_link]: <https://www.python.org/downloads/>
 
-[tvdb_api_link]: <https://github.com/dbr/tvdb_api>
 [pysrt_link]: <https://github.com/byroot/pysrt>
 [langdetect_link]: <https://github.com/Mimino666/langdetect>
 [pandas_link]: <http://pandas.pydata.org/>
-[numpy_link]: <http://www.numpy.org/>
+[numpy_link]: <https://dummy_link.com>
+[enum_link]: <https://dummy_link.com>
+[logging_link]: <https://dummy_link.com>
+[cython_link]: <https://dummy_link.com>
+[imdbpy_link]: <https://dummy_link.com>
+[python_mal_link]: <https://dummy_link.com>
+[tvdb_api_link]: <https://github.com/dbr/tvdb_api>
+[logging_link]: <https://dummy_link.com>
+[beautifulsoap4_link]: <https://dummy_link.com>
+[requests_link]: <https://dummy_link.com>
--- main/webparser/rarbg_lib.py
+++ main/webparser/rarbg_lib.py
@@ -1,147 +1,86 @@
 #!/usr/bin/python
 
-#from magnet2torrent import m2t_create
-#from simple_torrent_client import st_client
 
-# Load configuration File, Default Url, ProxyList
-# "For Updating"
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 
-# "<Default Url>" = https://www.rarbg.to/torrents.php?search=
+SHOW_FLAG = '6'
+FILM_FLAG = '14'
+ANIME_FLAG = '10'
 
-def rarbg_request_constructor (title, season, episode):
-    r = (title.replace(" ","%20") + '%20S'+ season + 'E' + episode)
-    print r
-    return (r)
+
+rarbg_default_url =  'https://www.rarbg.to/torrents.php?search='
+
+
+# TODO Devuelve None because reasons!!!
+def rarbg_request_builder(title='', year='', season='', episode='', subber='', fflag='0'):
+    if int(fflag) == SHOW_FLAG:
+        print rarbg_show_request_builder(title=title, season=season, episode=episode)
+    if int(fflag) == FILM_FLAG:
+        print rarbg_film_request_builder(title=title, year=year)
+    if int(fflag) == ANIME_FLAG:
+        print rarbg_anime_request_builder(title=title, episode=episode, subber=subber)
+
+
+def rarbg_film_request_builder(title='', year=''):
+    return (rarbg_default_url +  (title.replace(" ", "%20") + '%20' + str(year)))
+
+
+def rarbg_show_request_builder(title='', season='', episode=''):
+    return (rarbg_default_url + (title.replace(" ", "%20") + '%20S' + str(season) + 'E' + str(episode)) + '&category%5B%5D=18&category%5B%5D=41&category%5B%5D=49' )
+
+
+def rarbg_anime_request_builder(title='', episode='', subber=''):
+    return (rarbg_default_url + (str(subber) + '%20' + title.replace(" ", "%20") + '%20' + 'E' + str(episode)))
+
 
 def rarbg_search (url):
-    r = requests.get (url, verify=True)
-    return (r)
+    headers = {'UserAgent':str(UserAgent().random)}
+    try:
+        r = requests.get (url, verify=True, headers=headers)
+    except Exception as e:
+        print 'Unable to stablish connection'
+    finally:
+        if r.status_code == 200:
+            print '200 OK'
+            return r
 
 def rarbg_parser (content):
-    soup = BeautifulSoup (content.text, "html.parser")
-    table = soup.findAll('table', {"class":"lista2t"})
+    soup = BeautifulSoup (content.text, 'html.parser')
+    print soup.prettify()
+    ttable = soup.findAll('tr', {'class': 'lista2'})
+    print ttable
+    raw_input('Press [ENTER] To Continue...')
 
-    myMagnetList = []
-    mySeedList = []
-    myLeechList = []
-    mySizeList = []
-    myNameList = []
-
-    for row in table:
-        data_name = 
-        for name in data_name:
-            myNameList.append()
-            
-# Devuelve todo a modo de lista, la aproximacion que se me ocurre ahora
-# recuperar todos los elementos por posicion dentro de la propia lista recuperada
-# que seria recuperar la unidad de informacion, en este caso el bloque y luego iterar.
-
-# kat_request_constructor -> kat_request_builder():
-# **    -> "Unable to build de Request, missing Parameters"
-# rarbg_request_constructor -> rarbg_request_builder():
-
-# kat_search (url, magnet_flag):
-# rarbg_search (url, magnet_flag): 
-#   con el flag lo que busco es tener la busqueda standar de la lista
-#   a la par que puedo pedirle que busque el magnet del subrecurso y lo lance
-#   * Si no se consigue respuesta de la direccion principal, se intenta de los proxies
-#  ** que se conozcan -> "The web it's loading slow or not responding (<Default Url>)"
-#  **                   -> "Trying to reach out using some proxy (<Proxy Name>)"
-#  **                   -> "Unable to connect - Error (<Code Error>) - (404, 408, 500)"
-#  **                   -> ""
- 
-# rarbg_parser(content):
-#   **  -> Some values could not been retrieved (Setted to default values)"
-#   **  -> Unable to find (Name, Size, Seeds, Leechs, Magnet)
-#   retrieve: block list of 'td', retrieve the subvalues
-
-# Deberia crear una clase que tenga como objetos la lista de listas, sobre los datos
-# de los torrents que se hayan podido conseguir
-
-# recuperar el tpb_lib 
-
-# Externalizar el tratamiento de datos con pandas a otro modulo
-# El Modulo ese cargaria un fichero de configuración en el que se podran
-# settear criterios de busqueda (<Health>, <Size>, <hunt_flag>)
-
-# rarbg_batch_creator(content, A_flag):
-#   <- call for some info on nº of episodes
-#   <- inicializacion del bucle de busqueda.
-#   <- obtencion de lista preliminar
-#   <- seleccion de paquete
-#   <- Creacion y Almaceamiento de Magnet en la Biblioteca, Opcion Futura Compartir
-#   <- Overlay sobre la forma en la que se disponen las caratulas dentro del propio Xbmc
+    if ttable != []:
+    # TODO anadir un callback o algo si no lo encuentra de la primera forma.
+        print 'Retrieving individual values from the table'
+        for items in ttable:
+            title = (items.findAll('a')[1])['title']
+            print title
+            print '****' * 20
+    else:
+        print 'rarbg.to seems to not be working at the moment, please try again later'
 
 
-# Futuro: Crossreference los 3 resultados de los Trackers y crear aquella lista que
-# Tenga mejor media de salud.
-# Actualmente solo busco la funcionalidad individual de cada uno de los 3 parsers.
+
+def main():
+    show = rarbg_show_request_builder(title='Game Of Thrones', season='03', episode='01')
+    film =  rarbg_film_request_builder(title='Why Him', year='2016')
+    anime = rarbg_anime_request_builder(title='One Punch Man', episode='01', subber='HorribleSubs')
+
+    print show
+    print film
+    print anime
+    raw_input('Press [ENTER] To Launch WebScrapping...')
+
+    rarbg_parser(content=rarbg_search(show))
+    # rarbg_parser(content=rarbg_search(film))
+    # rarbg_parser(content=rarbg_search(anime))
 
 
-# Funcionamiento normal de esta libreria, se recibe una llamada a esta libreria
-# con la peticion sobre la busqueda de un contenido multimedia
-# el constructor valida y envia la peticion
-# la peticion es pasada al parser
-# inicializacion de la lista de objetos.
-# inicializacion del parseo
-# llamada a tratmiento de datos
-# obtencion de resultados preliminares
-# decision del usuario
-# cierre de uso de la libreria.
+if __name__ == '__main__':
+    main()
 
-
-# Tareas pendientes de los modulos de recuperacion de magnets
-#   * -> Hacer que permita la multi-descarga
-#   **  -> Añadir flags de control
-#   **  -> Añadir manejo de Excepciones
-
-
-# Refinar la linea de comandos, para que sea facil implementar una interfaz para el plugin
-# Siempre que se pueda CClient <-> Modulo Interfaz <-> Usuario
-
-# Mirar como implementar un MAN sencillo e ir construyendo la parte de la memoria + documentacion
-# para la memoria que requerira este proyecto.
-
-# Ir ordenando aquellos links relevantes para poder ponerlos en la bibliografia utilizada.
-# ** -> Con referencia a esto buscar el link que me ponia la expresion regular para volcar la
-# salida a un stdio especifico para poder pasearla y mostrarla en forma de barra de progreso.
-
-# Mirar ffmpeg, buscar una serie de presets ya echos, ya se refinaran luego (<Medium>, <High>)
-# añadir entrada en MAN sobre presets
-
-# Leer algo de como implementar el servicio de "<JellyFish>"
-# Lanzaria un servicio que conectaria los 2 nodos, Slave-Master type
-# Lanzando el reproductor que soporte esto, a ver si se puede hacer algo
-# con el reproductor que tiene integrado
-
-# Custom Filters
-# -> Setup de estos si es posible en un futuro
-
-# Posible distribucion de carga entre nodos de una misma red siempre y cuando tengan el
-# servicio corriendo
-
-# Sniffer, mirar de como implementar uno sencillo, que lo que haga sea cambiar el
-# numero de conexiones y velocidades de descarga en la red. Suponiendo que esta escuchando
-# una serie de puertos previamente configurados
-
-# Revisar como tratar los ficheros para la alineacion de audios.
-
-# Recupera Meta-Datos de IMBD, Rotten Tomatoes + Incrustarlos en el fichero.
-# Construir modulo que crea los arboles de directorios y renombra los ficheros
-
-# Scheluder para lanzar las tareas de forma periodica "(<Scheluder-Service>)"
-
-# Mirar como fucan los addons en xmbc, para poder trabajar en la parte del interface, que
-# a dia de hoy aun no tengo muy claro como voy a hacerla, necesitare ver que voy necesitado
-#
-# Aun asi deberia <Pantalla Carga> -> FST: <"Setting up the Environment">
-#                                   -> SND_X: Search Box, Configuration, "Catalog", "Recomended" 
-#                                   ->  Configuration:
-#                                   ->  "<Sniffer> <Scheluder> <Setup> <Others-About> <ffmpeg>"
-
-# Resultados en listas clickables
-#   -> Poner Colores en base a la salud??
-
-# Reconstruir el parser de kat, katproxy y algun proxie ya conocido
-# ahoy....
 
